298b3d3050cbf44874e079984671ee9a469bd688
Szapora Gyuszika <Szapora Gyuszika>
2016-08-15 21:15:27 +0300

    Added LoginController and Security

---
+++ starttobet/src/main/java/ro/sci/starttobet/controllers/SecurityFilter.java
@@ -0,0 +1,53 @@
+package ro.sci.starttobet.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ro.sci.starttobet.domain.User;
+import ro.sci.starttobet.service.SecurityService;
+
+public class SecurityFilter  implements Filter {
+
+	@Autowired
+	private SecurityService securityService;
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, 
+			ServletResponse response, 
+			FilterChain chain)
+			throws IOException, ServletException {
+		
+		User user =(User) ((HttpServletRequest)request).getSession().getAttribute("currentUser");
+		
+		securityService.
+		setCurrentUser(user);
+		
+		System.out.println("Thread name: " + Thread.currentThread().getName() +
+				", current user: " + (user != null ? user.getUserName() : null));
+		
+		chain.doFilter(request, response);
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ starttobet/src/main/java/ro/sci/starttobet/domain/Customer.java
@@ -0,0 +1,96 @@
+package ro.sci.starttobet.domain;
+
+import java.util.Date;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Customer extends AbstractModel {
+	
+	@NotEmpty
+	private String firstName;
+	
+	@NotEmpty
+	private String lastName;
+	
+	@NotEmpty
+	private String email;
+	
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private Date dateOfBirth;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", birthDate=" + dateOfBirth + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	
+
+}
